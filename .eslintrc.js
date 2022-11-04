0dc489cc9ffb3dc0001daa7ffca9e755977436ba
Mary Louise Hermosa <jayzces@gmail.com>
2022-11-05 00:07:40 +0800

    Project initialization

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'plugin:tailwindcss/recommended',
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'tailwindcss/classnames-order': 'warn',
+    'tailwindcss/enforces-negative-arbitrary-values': 'warn',
+    'tailwindcss/enforces-shorthand': 'warn',
+    'tailwindcss/migration-from-tailwind-2': 'warn',
+    'tailwindcss/no-arbitrary-value': 'off',
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/no-contradicting-classname': 'error',
+    'vue/no-v-html': 'off',
+    'no-unused-vars': 'warn',
+  },
+}
